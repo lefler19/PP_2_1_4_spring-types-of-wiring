6cf89fbc748aeb7f086ef440f1a0220db178d7ad
Настя <west_2017@mail.ru>
2023-09-21 14:06:43 +0300

    2.1.5

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-
-    @Override
-    public String toString() {
-        return ", в утке яйцо " + "";
-    }
+private Egg6 egg;
+@Override
+public String toString() {
+    return ", в утке яйцо " + egg.toString();
+}
+@Autowired
+public Duck5(Egg6 egg) {
+this.egg = egg;
+}
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,17 @@
 package koschei.models;
 
-public class Egg6 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-    @Override
-    public String toString() {
-        return ", в яйце иголка " + "";
-    }
+@Component
+public class Egg6 {
+    private Needle7 needle;
+@Autowired
+ public void getNeedle(Needle7 needle) {
+    this.needle = needle;
+}
+@Override
+public String toString() {
+    return ", в яйце иголка " + needle.toString();
+}
 }
